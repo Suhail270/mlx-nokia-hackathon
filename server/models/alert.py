92a8848ab0190cf3bfe4a36afbb46b193769d7ad
Suhail Ahmed <suhailz13ahmed@outlook.com>
2025-02-04 23:40:31 +0400

    manually syncing with stable to resolve pr - part 2

--- server/models/alert.py
+++ server/models/alert.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, String, Float
+from sqlalchemy import Column, String, Float, Integer, Boolean, ForeignKey
+from sqlalchemy.orm import relationship
 from db.session import Base
 
 class Alert(Base):
@@ -14,3 +15,53 @@ class Alert(Base):
     description = Column(String)
     image = Column(String)
     video_path = Column(String)
+    geo_info = Column(String)
+    responder_type = Column(String)
+    response_time = Column(Integer)
+    resolution_time = Column(Integer)
+    status = Column(String)
+    dispatch = relationship("Dispatch", back_populates="alert", cascade="all, delete-orphan")
+
+
+class Police(Base):
+    __tablename__ = "police"
+
+    id = Column(String, primary_key=True, index=True)
+    zone = Column(String)
+
+class Firefighter(Base):
+    __tablename__ = "firefighter"
+
+    id = Column(String, primary_key=True, index=True)
+    zone = Column(String)
+
+class Ambulance(Base):
+    __tablename__ = "ambulance"
+
+    id = Column(String, primary_key=True, index=True)
+    zone = Column(String)
+
+
+class Drone(Base):
+    __tablename__ = "drone"
+
+    id = Column(String, primary_key=True, index=True)
+    zone = Column(String)
+
+class Dispatch(Base):
+    __tablename__ = "dispatch"
+
+    id = Column(String, primary_key=True, index=True)
+    alert_id = Column(String, ForeignKey("alerts.id"))
+    police_id = Column(String, ForeignKey("police.id"))
+    ambulance_id = Column(String, ForeignKey("ambulance.id"))
+    firefighter_id = Column(String, ForeignKey("firefighter.id"))
+    drone_id = Column(String, ForeignKey("drone.id"))
+    dispatch_time = Column(String)
+
+    alert = relationship("Alert", back_populates="dispatch")
+    police = relationship("Police")
+    ambulance = relationship("Ambulance")
+    firefighter = relationship("Firefighter")
+    drone = relationship("Drone")
+
