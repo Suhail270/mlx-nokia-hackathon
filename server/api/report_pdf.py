6e912f9cf4687cc3171a30942abe010924f32baa
MHMujtaba <mujtaba.mohimtulay@gmail.com>
2025-02-06 01:34:03 +0400

    edited pdf look

--- server/api/report_pdf.py
+++ server/api/report_pdf.py
@@ -22,7 +22,7 @@ load_dotenv()
 NVIDIA_API_KEY = os.getenv("NVIDIA_API_KEY")
 
 LLM_MODEL = "meta/llama-3.1-70b-instruct"
-SUMMARIZER_MODEL = "deepseek-ai/deepseek-r1"
+SUMMARIZER_MODEL = "meta/llama-3.1-8b-instruct"
 
 def extract_post_think_content(text: str) -> str:
     """Extract content after </think> tag."""
@@ -31,27 +31,41 @@ def extract_post_think_content(text: str) -> str:
         return match.group(1).strip()
     return text
 
-def engage_llm(prompt):
+def engage_llm(prompts):
+    sections=[]
     try:
-        messages = [
-            {"role": "system", "content": "You are an AI assistant generating a periodic incident report."},
-            {"role": "user", "content": prompt}
-        ]
-        response = llm_client.chat.completions.create(
-            model=SUMMARIZER_MODEL,
-            messages=messages,
-            temperature=0.6,
-            top_p=0.7,
-            max_tokens=4096,
-            stream=False
-        )
-        summary = response.choices[0].message.content.strip()
-        processed_summary = extract_post_think_content(summary)
-        return processed_summary
+        for prompt in prompts:
+            messages = [
+                {"role": "system", "content": "You are an AI assistant generating a periodic incident report."},
+                {"role": "user", "content": prompt}
+            ]
+            response = llm_client.chat.completions.create(
+                model=SUMMARIZER_MODEL,
+                messages=messages,
+                temperature=0.2,
+                top_p=0.7,
+                max_tokens=1024,
+                stream=True
+
+            )
+            # print("here")
+            # print("response is ",response.choices[0].message.content)
+            # summary = response.choices[0].message.content.strip()
+            # su = "fdas"
+            summary=""
+            for chunk in response:
+                if chunk.choices[0].delta.content is not None:
+                    summary=summary+chunk.choices[0].delta.content
+                    # print("break here!!")
+                    print(chunk.choices[0].delta.content, end="")
+            
+            processed_summary = extract_post_think_content(summary)
+            sections.append(processed_summary)
+        return sections
     except Exception as e:
         # logger.error(f"Error generating summary: {e}")
         print("error")
-        # raise HTTPException(status_code=500, detail="Error generating summary.")
+        raise HTTPException(status_code=500, detail="Error generating summary.")
 
 embedding_client = OpenAI(api_key=NVIDIA_API_KEY, base_url="https://integrate.api.nvidia.com/v1")
 llm_client = OpenAI(api_key=NVIDIA_API_KEY, base_url="https://integrate.api.nvidia.com/v1")
@@ -88,7 +102,7 @@ def create_reports(interval ,db=Depends(get_db)):
     incidents=[]
     for json_obj in data:
         filtered_json = {key: json_obj[key] for key in keys_to_keep if key in json_obj}
-        print("curr->",filtered_json)
+        # print("curr->",filtered_json)
         incidents.append(filtered_json)
     
     
@@ -105,26 +119,37 @@ def create_reports(interval ,db=Depends(get_db)):
 def generate_report_from_data(incidents):
 
     # Create the prompt for Gen AI
-    prompt = f"""
-    You are an AI assistant generating a periodic (defined in number of days) incident report. Be straight forward and maintain a structure that can be placed into a pdf file. Do not talk to me. Only give me the text that would be put in the PDF.
-    Analyze the following incident data and summarize key insights:
+    prompts=[]
+    headings=["Total incidents","Most common incident type","Severity distribution","Locations with the highest incidents","Open vs closed case summary","Key observations & recommendations"]
+    for heading in headings:
 
-    {incidents}
+        # prompt = f"""
+        # You are an AI assistant generating a periodic (defined in number of days) incident report. Be straight forward and maintain a structure that can be placed into a pdf file.
+        # Analyze the following incident data and summarize:
 
-    Generate a structured report with:
-    - Total incidents
-    - Most common incident type
-    - Severity distribution
-    - Locations with the highest incidents
-    - Open vs closed case summary
-    - Key observations & recommendations
-    """
-    return prompt
+        # {incidents}
 
-def save_report_to_pdf(report_text, filename="./reports/pdf/incident_report.pdf"):
+        # Generate a report summary with the {heading}
+        # Add no other information.
+        
+        # """
+
+        prompt = f"""
+        You are an AI assistant generating an incident report. Analyse the following incident data and generate only the {heading} section of the report:
+
+        {incidents}
+
+        """
+        prompts.append(prompt)
+   
+    return prompts
+
+def save_report_to_pdf(report_texts, filename="./reports/pdf/incident_report.pdf"):
     """Generate a PDF file with a bordered report."""
     c = canvas.Canvas(filename, pagesize=letter)
     width, height = letter
+    
+
     c.setFont("Helvetica", 12)
 
     margin_x = 40
@@ -139,33 +164,70 @@ def save_report_to_pdf(report_text, filename="./reports/pdf/incident_report.pdf"
     # Write text inside the border
     y_position = height - margin_y - 20  # Start position for text
     line_height = 14  # Spacing between lines
-    max_line_length = 80
-    wrapped_lines = []
-    for line in report_text.split("\n"):
-        wrapped_lines.extend(textwrap.wrap(line, width=max_line_length))
-
-    for line in wrapped_lines:
-        words = re.split(r"(\*\*.*?\*\*)", line)  
-        x_position = margin_x + 10
-
-        for word in words:
-            if word.startswith("**") and word.endswith("**"):
-                c.setFont("Helvetica-Bold", 12)  # Apply bold font
-                word = word[2:-2]  # Remove **
+    max_line_length = 90
+    title=0
+    
+    for report_text in report_texts:
+        
+        wrapped_lines = []
+        for line in report_text.split("\n"):
+            if line.startswith("* "):
+                ls= textwrap.wrap(line, width=max_line_length)
+                wrapped_lines.append(ls[0])
+                for i in range(1,len(ls)):
+                    wrapped_lines.append("* "+ls[i])
             else:
-                c.setFont("Helvetica", 12)  # Normal font
+                wrapped_lines.extend(textwrap.wrap(line, width=max_line_length))
 
-            c.drawString(x_position, y_position, word)
-            x_position += c.stringWidth(word, c._fontname, 12) + 5  # Move right
+        for line in wrapped_lines:
+            words = re.split(r"(\*\*.*?\*\*)", line)  
+            x_position = margin_x + 10
 
-        y_position -= line_height
+            for word in words:
+                if word.startswith("**") and word.endswith("**"):
+                    c.setFont("Helvetica-Bold", 12)  # Apply bold font
+                    word = word[2:-2]  # Remove **
+                elif word.startswith("* "):
+                    c.setFont("Times-Italic", 12)  # Apply bold font
+                    word = word[1:]
+                else:
+                    c.setFont("Helvetica", 12)  # Normal font
+
+                c.drawString(x_position, y_position, word)
+
+                x_position += c.stringWidth(word, c._fontname, 12) + 5  # Move right
+
+            y_position -= line_height
+            if y_position < margin_y + 20:  # Create new page if needed
+                c.showPage()
+                c.setFont("Helvetica", 12)
+                y_position = height - margin_y - 20
+                c.setStrokeColor(colors.black)
+                c.setLineWidth(2)
+                c.rect(margin_x, margin_y, text_width, text_height)
+        if title==1:
+            image_path = os.path.abspath("api/graphs/type.jpg")
+            c.drawImage(image_path, 100, y_position -150, width=200, height=150)  
+            image_path = os.path.abspath("api/graphs/status.jpg")
+            c.drawImage(image_path, 350, y_position -150, width=200, height=150)  
+
+            y_position -= (150+line_height)
+        elif title==2:
+            image_path = os.path.abspath("api/graphs/severity.jpg")
+            c.drawImage(image_path, 100, y_position -150, width=200, height=150) 
+            y_position -= (150+line_height)
+        title+=1
+        c.drawString(x_position, y_position, "")
+        y_position -= 20
         if y_position < margin_y + 20:  # Create new page if needed
-            c.showPage()
-            c.setFont("Helvetica", 12)
-            y_position = height - margin_y - 20
-            c.setStrokeColor(colors.black)
-            c.setLineWidth(2)
-            c.rect(margin_x, margin_y, text_width, text_height)
+                c.showPage()
+                c.setFont("Helvetica", 12)
+                y_position = height - margin_y - 20
+                c.setStrokeColor(colors.black)
+                c.setLineWidth(2)
+                c.rect(margin_x, margin_y, text_width, text_height)
+        
+    
 
     directory = "./reports/pdf" 
     if not os.path.exists(directory):
